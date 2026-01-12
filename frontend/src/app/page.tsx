42ae1c613275dc955b60c8da8a87761c5881247b
Bhanuka Gihan <bhanukagihan4@gmail.com>
2026-01-12 23:15:48 +0530

    feat: refactor Home component to LoadingPage with loading state and background styling

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,30 @@
+'use client'
 
-export default function Home() {
+import { useEffect,useState } from "react";
+import { motion } from "framer-motion";
+
+
+//import components here
+import home from './Home/page'
+
+
+
+
+export default function LoadingPage() {
+
+const[loading,setLoading]=useState(true);
+
+
+ 
   return (
-  <h1 className="text-white">hello </h1>
-  );
+<div className="min-h-screen w-full relative flex flex-col justify-center items-center ">
+    <div
+    className="absolute inset-0 z-0"
+    style={{
+      background: "radial-gradient(125% 125% at 50% 10%, #000000 40%, #010133 100%)",
+    }}
+  />
+ 
+</div>
+  )
 }
