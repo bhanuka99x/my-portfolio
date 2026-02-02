66acee0a5c2563bdc5be2af565c0bcc5157998dd
Bhanuka Gihan <bhanukagihan4@gmail.com>
2026-02-02 15:11:38 +0530

    feat: add skill constants for improved skill management in AboutSection

---
+++ frontend/src/constants/about.ts
@@ -0,0 +1,18 @@
+import { skillProgress } from "../types";
+
+export const skill: skillProgress[] = [
+    {
+      name: "Frontend Development",
+      level: 85,
+      icon: "Code2"
+    },
+    { name: "UI/UX Design", 
+      level: 88, 
+      icon: "Palette" },
+    {
+      name: "Backend Development",
+      level: 95,
+      icon: "Code2",
+    },
+]
+
