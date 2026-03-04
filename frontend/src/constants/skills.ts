93c9cff1c9d9a6581fb889d8a0e1b7788a29e590
Bhanuka Gihan <bhanukagihan4@gmail.com>
2026-03-04 22:04:31 +0530

    feat: update skillsData to include additional skills in various categories

--- frontend/src/constants/skills.ts
+++ frontend/src/constants/skills.ts
@@ -2,17 +2,17 @@ export const skillsData = [
   {
     id: 1,
     category: "Frontend // Interface",
-    skills: ["React.js", "Next.js", "TypeScript", "JavaScript", "Tailwind CSS", "HTML", "State Management", "User Interface (UI) Design", "User Experience (UX)", "Figma (Software)", "Chrome Extensions", "XML"],
+    skills: ["React.js", "Next.js", "TypeScript", "JavaScript", "Tailwind CSS", "HTML", "State Management", "User Interface (UI) Design", "User Experience (UX)", "Figma (Software)", "Chrome Extensions", "Framer Motion","Recharts.js", "Chrome Extension MV3","Socket.IO Client"],
   },
   {
     id: 2,
     category: "Backend // Logic",
-    skills: ["Node.js", "Express.js", "Spring Boot", "C#", ".NET Framework", "Java", "Systems Design", "API Development", "REST APIs", "WebSocket", "JSON Web Token (JWT)", "User Authentication", "Software Documentation"],
+    skills: ["Node.js", "Express.js", "Spring Boot", "Java", "Systems Design", "API Development", "REST APIs", "WebSocket", "JSON Web Token (JWT)", "User Authentication", "Software Documentation","Socket.IO (Server-side)"],
   },
   {
     id: 3,
     category: "Data // Systems",
-    skills: ["PostgreSQL", "MySQL", "Redis", "Database Design & Management", "Object-Relational Mapping (ORM)", "Sequelize.js", "Data Caching", "Queue Management", "Firebase", "supabase", "supabase Auth"],
+    skills: ["PostgreSQL", "MySQL", "Redis", "Database Design & Management", "Object-Relational Mapping (ORM)", "Sequelize.js", "Data Caching", "Queue Management", "Firebase", "supabase", "supabase Auth","AWS S3"],
   },
   {
     id: 4,
@@ -22,6 +22,6 @@ export const skillsData = [
   {
     id: 5,
     category: "Quality // Intelligence",
-    skills: ["Jest", "Playwright", "Web Application Security", "AI Agents", "GitHub Copilot", "Postman API", "DSA", "Object-Oriented Programming (OOP)", "Collaborative Problem Solving"],
+    skills: ["Jest", "Playwright", "Web Application Security", "AI Agents", "GitHub Copilot", "Postman API", "DSA", "Object-Oriented Programming (OOP)", "Collaborative Problem Solving","Performance Optimization (Redis Caching)","Atomic Transactions "],
   },
 ];
